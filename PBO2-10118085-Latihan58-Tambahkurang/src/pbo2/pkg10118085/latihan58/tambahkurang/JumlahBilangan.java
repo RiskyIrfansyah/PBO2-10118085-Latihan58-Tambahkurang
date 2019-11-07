5ea07c88128f18d51432c8c3fa86c295502cdf26
Risky Irfansyah <55643683+RiskyIrfansyah@users.noreply.github.com>
2019-11-07 19:47:15 +0700

    Add files via upload

---
+++ PBO2-10118085-Latihan58-Tambahkurang/src/pbo2/pkg10118085/latihan58/tambahkurang/JumlahBilangan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118085.latihan58.tambahkurang;
+
+/**
+ *
+ * @author
+ * NAMA     : Risky Irfansyah
+ * KELAS    : IF-2
+ * NIM      : 10118085
+ * Deskripsi Program : Membuat program dengan pendekatan berbasis objek untuk menghitung
+ * penjumlahan dan selisih nilai
+ */
+public class JumlahBilangan extends Bilangan {
+    public void tampilHasilJumlah() {
+        System.out.println("Hasil perjumlahan = " + (getX() + getY()));
+    }
+
+}
